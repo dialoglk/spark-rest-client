f3186d41652724c5b914a15dff6405d463f2d6dd
Tharaka Devinda <tharaka@vista.aero>
2018-09-25 16:15:29 +0530

    added queue function for queries

--- src/main/java/lk/dialog/analytics/spark/models/JobResponse.java
+++ src/main/java/lk/dialog/analytics/spark/models/JobResponse.java
@@ -1,7 +1,11 @@
 package lk.dialog.analytics.spark.models;
 
+import com.google.gson.JsonArray;
+
 public class JobResponse {
     private boolean success;
+    private boolean timedout;
+    private JsonArray data;
     private Integer id;
 
     public boolean isSuccess() {
@@ -18,5 +22,21 @@ public class JobResponse {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public boolean isTimedout() {
+        return timedout;
+    }
+
+    public void setTimedout(boolean timedout) {
+        this.timedout = timedout;
+    }
+
+    public JsonArray getData() {
+        return data;
+    }
+
+    public void setData(JsonArray data) {
+        this.data = data;
     }
 }
